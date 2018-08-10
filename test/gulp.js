865d9ba820363ad073d412145d78ed20b6335485
Vincent Weevers <mail@vincentweevers.nl>
2018-08-10 11:05:14 +0200

    fix: tests

--- test/gulp.js
+++ test/gulp.js
@@ -5,62 +5,16 @@ const test = require('tape')
     , u = require('./util')
     , run = require('./util/runner')('generators/gulp')
 
-test('esnext', (t) => {
-  const ES6 = ['gulpfile.babel.js', '.babelrc']
-      , ES5 = ['gulpfile.js']
-
-  t.test('esnext option true', (t) => {
-    t.plan(2)
-    run({ options: { esnext: true }}, () => {
-      u.files(t, ES6)
-      u.notFiles(t, ES5)
-    })
-  })
-
-  t.test('esnext option false', (t) => {
-    t.plan(2)
-    run({ options: { esnext: false }}, () => {
-      u.files(t, ES5)
-      u.notFiles(t, ES6)
-    })
-  })
-
-  t.test('esnext prompt true', (t) => {
-    t.plan(2)
-    run({ prompts: { esnext: true }}, () => {
-      u.files(t, ES6)
-      u.notFiles(t, ES5)
-    })
-  })
-
-  t.test('esnext prompt false', (t) => {
-    t.plan(2)
-    run({ prompts: { esnext: false }}, () => {
-      u.files(t, ES5)
-      u.notFiles(t, ES6)
-    })
-  })
-
-  t.test('esnext option overrides prompt', (t) => {
-    t.plan(2)
-
-    run({options: { esnext: true }, prompts: { esnext: false }}, () => {
-      u.files(t, ES6)
-      u.notFiles(t, ES5)
-    })
-  })
-})
-
-test('creates example gulp task', (t) => {
+test('creates gulpfile and example gulp task', (t) => {
   t.plan(1)
-  run(() => u.files(t, ['tasks/build.js']))
+  run(() => u.files(t, ['gulpfile.js', 'tasks/build.js']))
 })
 
 test('saves dependencies', (t) => {
   t.plan(1)
 
   run({ options: { esnext: true } }, () => {
-    let expected = ['gulp', 'gulp-util', 'glob', 'babel-core']
+    let expected = ['gulp']
       , pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
       , actual = Object.keys(pkg.devDependencies)
 
